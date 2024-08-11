a5387235b2bd815011e41666977bb90f20ffdf28
Nitin <55267590+nitincpc@users.noreply.github.com>
2024-08-11 09:21:02 +0530

    New Simple Github Actions Print Hello

---
+++ Script.py
@@ -0,0 +1,10 @@
+# script.py
+
+import datetime
+
+def main():
+    print("Hello, GitHub Actions!")
+    print(f"Current date and time: {datetime.datetime.now()}")
+
+if __name__ == "__main__":
+    main()
